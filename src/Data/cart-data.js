d01c99c83e1d706551da6614a270440d55e0506a
aryawirasandi <aryawirasandi7153@gmail.com>
2020-06-13 01:11:28 +0700

    [data component] 1. change data naming and key

--- src/Data/cart-data.js
+++ src/Data/cart-data.js
@@ -1,36 +1,36 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
- const CartItems = [
+const CartItems = [
     {
-        id : uuidv4(),
-        name : "Car",
-        category : "Toy",
-        quantity : 4,
+        id: uuidv4(),
+        name: "Car",
+        category: "Toy",
+        quantity: 4,
     },
     {
-        id : uuidv4(),
-        name : "Harry Potter And Sorrority Stone",
+        id: uuidv4(),
+        name: "The way of life",
         category: "Book",
-        quantity:  5
+        quantity: 5
     },
     {
-        id : uuidv4(),
-        name : "Nutricia",
-        category:  "Food",
-        quantity:  2,
+        id: uuidv4(),
+        name: "Lucky Biscuit",
+        category: "Food",
+        quantity: 2,
     },
     {
-        id : uuidv4(),
-        name : "Lactamil",
+        id: uuidv4(),
+        name: "Mommy Help",
         category: "Pregnant Milk",
         quantity: 15
     },
     {
-        id : uuidv4(),
-        name : "Lemari serba guna 4 in 1",
+        id: uuidv4(),
+        name: "New Briefcase 2020",
         category: "assets"
     }
 ];
 
- export default CartItems;
+export default CartItems;
 

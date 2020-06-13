17cfce41147ea6465c8420686e812388cc892ab2
aryawirasandi <aryawirasandi7153@gmail.com>
2020-06-13 20:42:42 +0700

    [data component] 1. update quantity 2. change price 3. add new img assets and import to data

--- src/Data/cart-data.js
+++ src/Data/cart-data.js
@@ -1,34 +1,54 @@
 import {v4 as uuidv4} from 'uuid';
-
+import BuscuitLogo from '../images/contents/biscuits.jpg';
+import CarToyLogo from '../images/contents/car.jpg';
+import BookLogo from '../images/contents/books.jpg';
+import MilkLogo from '../images/contents/pregnant-milk.jpg';
+import BriefCaseLogo from '../images/contents/briefcase.jpg'
 const CartItems = [
     {
         id: uuidv4(),
         name: "Car",
         category: "Toy",
         quantity: 4,
+        url : CarToyLogo,
+        propName : "car-image",
+        price : 50000
     },
     {
         id: uuidv4(),
         name: "The way of life",
         category: "Book",
-        quantity: 5
+        quantity: 5,
+        url : BookLogo,
+        propName : "Book-image",
+        price : 250000
     },
     {
         id: uuidv4(),
         name: "Lucky Biscuit",
         category: "Food",
         quantity: 2,
+        url : BuscuitLogo,
+        propName : "biscuit-image",
+        price : 8000
     },
     {
         id: uuidv4(),
         name: "Mommy Help",
         category: "Pregnant Milk",
-        quantity: 15
+        quantity: 15,
+        url : MilkLogo,
+        propName : "milk-image",
+        price : 50000
     },
     {
         id: uuidv4(),
         name: "New Briefcase 2020",
-        category: "assets"
+        category: "assets",
+        quantity: 29,
+        url: BriefCaseLogo,
+        propName : "briefcase-image",
+        price : 1500000
     }
 ];
 

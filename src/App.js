e0f504c367576bf37bbdda7931774cd9b3f71168
aryawirasandi <aryawirasandi7153@gmail.com>
2020-06-22 05:29:15 +0700

    [Footer Component] 1. Add Footer Component 2. Add new Footer Component to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Card from "./components/CardContainer/Card";
 import './index.css';
 import NavbarComponent from "./components/Navbar/NavbarComponent.js"
+import ListItemComponent from "./components/ListItem/ListItemComponent.js";
+import Footer from './components/FooterComponent/FooterComponent.js';
 function App() {
   return (
-        <Card>
-          <NavbarComponent/>
-        </Card>
+        <React.Fragment>
+            <Card>
+                <NavbarComponent/>
+                <ListItemComponent/>
+            </Card>
+            <Footer title="Add To Cart"/>
+        </React.Fragment>
   );
 }
 

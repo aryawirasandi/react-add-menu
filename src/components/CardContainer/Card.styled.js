1d321a885d7fbfdab5765e87cfde9158075ddb89
aryawirasandi <aryawirasandi7153@gmail.com>
2020-06-22 05:26:42 +0700

    [Card Component] 1. Update Height of card container 2. update width of card container for laptop breakpoint

--- src/components/CardContainer/Card.styled.js
+++ src/components/CardContainer/Card.styled.js
@@ -4,14 +4,14 @@ import device  from '../../Style/Breakpoint';
 const CardContainer = styled.div`
   display: block;
   box-shadow: 10px 10px 12px -9px rgba(236,236,236,0.56);
-  height: 100vh;
+  min-height: 100vh;
   background: #f8f8f8;
   margin: 0 auto;
   @media ${device.mobileSmall}{
         margin: 0 auto;
   }
   @media ${device.laptop}{
-        width : 30vw;
+        width : 480px;
         margin : 0 auto;
         box-shadow: 10px 10px 12px -9px rgba(0,0,0,0.56);
   }
